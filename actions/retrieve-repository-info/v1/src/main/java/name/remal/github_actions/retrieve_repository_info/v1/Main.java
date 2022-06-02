92d1e70045b629d545c332453895e2417cbbb62f
Semyon Levin <levin.semen@gmail.com>
2022-06-01 18:15:10 -0700

    test

--- actions/retrieve-repository-info/v1/src/main/java/name/remal/github_actions/retrieve_repository_info/v1/Main.java
+++ actions/retrieve-repository-info/v1/src/main/java/name/remal/github_actions/retrieve_repository_info/v1/Main.java
@@ -1,27 +1,13 @@
 package name.remal.github_actions.retrieve_repository_info.v1;
 
-import static name.remal.github_actions.utils.Commands.forLogGroup;
-
 import lombok.extern.log4j.Log4j2;
-import lombok.val;
 import name.remal.github_actions.common.lifecycle.MainLifecycle;
 
 @Log4j2
 public class Main {
 
     @MainLifecycle
-    public static void main() throws Throwable {
-        forLogGroup("group", () -> {
-            forLogGroup("inner group", () -> {
-                log.debug("debug\n1\n2", new RuntimeException());
-                log.info("info\n1\n2", new RuntimeException());
-                log.warn("warn\n1\n2", new RuntimeException());
-                log.error("error\n1\n2", new RuntimeException());
-            });
-        });
-        val thread = new Thread(() -> log.error("error", new RuntimeException()));
-        thread.run();
-        thread.join();
+    public static void main() {
     }
 
 }
